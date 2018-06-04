dda41ae381ab9b2bbb05e60932c70eac4108f42b
root <taberlick@gmail.com>
2018-06-04 17:31:22 +0300

    Request exception handled

--- src/main/java/com/telegram/api/App.java
+++ src/main/java/com/telegram/api/App.java
@@ -3,18 +3,18 @@ package com.telegram.api;
 import com.telegram.bot.Bot;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.*;
+
 
 public class App {
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi botapi = new TelegramBotsApi();
-
         try {
             botapi.registerBot(new Bot());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+        } catch (TelegramApiRequestException e) {
+            throw new RuntimeException(e);
         }
     }
 
